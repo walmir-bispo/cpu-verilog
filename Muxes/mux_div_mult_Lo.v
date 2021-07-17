904aebfc3ffd00a2f07f3f8742d7f402355faad8
Isaac Ferreira <ifs5@cin.ufpe.br>
2021-07-17 12:37:27 -0300

    Add files via upload

---
+++ Muxes/mux_div_mult_Lo.v
@@ -0,0 +1,9 @@
+module mux_div_mult_Lo (
+    input wire [1:0] selector,
+    input wire [31:0] data_0,
+    input wire [0:31] data_1,
+    output wire [31:0] Data_out
+);
+    assign Data_out = (selector) ? data_0 : data_1
+    
+endmodule
