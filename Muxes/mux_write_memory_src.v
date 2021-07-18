3c470895d6a6a0366739154ef4bf91a0729f4152
Walmir Bispo <55456025+wbsj14@users.noreply.github.com>
2021-07-18 12:52:33 -0300

    Update mux_write_memory_src

--- Muxes/mux_write_memory_src.v
+++ Muxes/mux_write_memory_src.v
@@ -1,11 +1,11 @@
 module mux_writememorysrc (
     
-    input wire    [1:0] selector,   
+    input wire    selector,   
     input wire    [31:0] data_0,    
     input wire    [31:0] data_1, 
     output wire   [31:0] Data_out
 );
-    assign Data_out = (selector) ? data_0 : data_1
+    assign Data_out = (selector) ? data_0 : data_1;
     
     
 endmodule
