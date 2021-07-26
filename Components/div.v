29e9d1e3cae79e6a7beb9cc9c9e3f870c93b1c3f
douglasst2 <55150864+douglasst2@users.noreply.github.com>
2021-07-26 15:11:43 -0300

    Add files via upload

--- Components/div.v
+++ Components/div.v
@@ -1,30 +1,81 @@
-module div(input wire[31:0] Data_a,
-            input wire [31:0] Data_b,
+module div(input wire[31:0] Dividendo,
+            input wire [31:0] Divisor,
             input wire reset,
             input wire clk,
-            output wire [31:0] lo,
-            output wire [31:0] hi,
-            output reg exception);
+            output wire [31:0] LO,
+            output wire [31:0] HI,
+            output wire exception
+			);
 
+  reg[63:0] Resto;
+  reg[5:0] Counter;
+  reg[31:0] auxDividendo;
+  reg[63:0] auxDivisor;
+  reg[31:0] auxException;
+  reg[31:0] auxQuociente;
+  reg seletor;
+  reg seletorException;
   reg [31:0] auxLO;
   reg [31:0] auxHI;
+ 
 
-always @(posedge clk)
-begin
-  if(reset)
-  begin
-    auxLO <= 32'd0;
-    auxHI <= 32'd0;
-  end
-  if(Data_b == 32'd0)
-  begin
-    exception <= 1;
-    end
-    auxLO <= Data_a/Data_b;
-    auxHI <= Data_a%Data_b;
-
+initial begin
+  auxDividendo = Dividendo;
+  auxDivisor[63:31] = Divisor;
+  auxException = Divisor;
+  Resto = Dividendo;
 end
-  assign lo = auxLO;
-  assign hi = auxHI;
+
+always @(posedge clk or Dividendo or Divisor or reset)
+begin
+  if(reset == 1'b1)begin
+    Resto = 64'd0;
+    Counter = 6'd0;
+    auxDividendo = 32'd0;
+    auxDivisor = 64'd0;
+    auxQuociente = 32'd0;
+    seletor = 1'b0;
+	 seletorException = 1'b0;
+	 auxException = 32'd0;
+    end
+	 else begin 
+	 Resto = auxDivisor - Resto;
+	end
+  
+    if(Resto >= 0)begin 
+        auxQuociente = auxQuociente<<1;
+        auxQuociente[0] = 1'b1;
+
+        auxDivisor = auxDivisor>>1;
+        Counter = Counter + 1'b1;
+    end
+    else begin 
+        Resto = auxDivisor + Resto;
+        auxQuociente = auxQuociente<<1;
+        auxQuociente[0] = 1'b0;
+    
+        
+        Counter = Counter + 1'b1;
+    end 
+	 
+	 auxDivisor = auxDivisor>>1;
+
+    if(Counter == 6'b0100001)begin
+		auxHI =  Resto[31:0];
+		auxLO =  auxQuociente[31:0];
+		seletor = 1;
+  if(auxException == 32'd0)begin
+		seletorException = 1;
+  end
+end
+end
+
+assign HI = (seletor) ? auxHI:HI;
+assign LO = (seletor) ? auxLO:LO;
+assign exception = seletorException ? 1:0;
+
+
+
+
 
 endmodule
