71597ab912256185a5f07c5cd85f760952398775
Walmir Bispo <55456025+wbsj14@users.noreply.github.com>
2021-07-31 15:36:38 -0300

    Delete Muxes directory

--- Muxes/muxDivMult.v
+++
@@ -1,9 +0,0 @@
-module muxDivMult(
-    input wire controlDivMult,
-    input wire [31:0] LO,
-    input wire [31:0] HI,
-    output wire [31:0] Data_out
-);
-    assign Data_out = controlDivMult ? (LO):(HI);
-
-endmodule
